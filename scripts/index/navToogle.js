5e1dab0e4820a7067415feed0faabc423929b424
ElielBejar <elielbejar12@gmail.com>
2024-02-29 09:54:05 -0300

    diseño index terminado

---
+++ scripts/index/navToogle.js
@@ -0,0 +1,13 @@
+const toogle = document.getElementById("toogle");
+const options = document.getElementById("options");
+toogle.addEventListener("click", navToogle);
+
+
+function navToogle(){
+    leftOptions = options.style.left;
+    if(leftOptions == "-50%"){
+        options.style.left = "0";
+    }else{
+        options.style.left = "-50%";
+    }
+}
